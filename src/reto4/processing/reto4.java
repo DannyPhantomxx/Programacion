5c54515a014f715510b5b2b8eb8a8402f48b7010
DannyPhantomxx <al04500293@tecmilenio.mx>
2022-09-10 16:53:47 -0500

    reto4

--- src/reto4/processing/reto4.java
+++ src/reto4/processing/reto4.java
@@ -1,43 +1,74 @@
-
 package reto4.processing;
 
 import java.io.*;
-
-public class reto4 {
+class tabla1
+{
     public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+    public static void main (String[] args) throws IOException
+    {
 
-    public static void main(String[ ] args)throws IOException {
+        int trimestre;
+        int chocolate = 0, vainilla = 0, fresa = 0, oreo = 0;
 
-        //Arreglos
-        String[] Sabores = {"Chocolate", "Vainilla", "Fresa", "Oreo"};
-        String[] Fechas = {"Ene-Feb-Mar", "Abr-May-Jun", "Jul-Ago-Sep", "Oct-Nov-Dic"};
+        int [][] tabla = new int[4][4];
 
-        int[][] ventas = {{111, 483, 427},
-                {192, 500, 355, 158},
-                {289, 470, 474, 160},
-                {415, 114, 161, 308}};
+        tabla [0] [0] = 111;
+        tabla [1] [0] = 192;
+        tabla [2] [0] = 289;
+        tabla [3] [0] = 415;
 
-        String FECH = "";//Almacenamiento de fecha
-        String SAB = "";//Almacena sabores
+        tabla [0] [1] = 483;
+        tabla [1] [1] = 500;
+        tabla [2] [1] = 470;
+        tabla [3] [1] = 114;
 
-        int contador2 = 0;
-        int minitrim = 0;
-        int opera1 = 0;
-        int opera2 = 0;
-        int opera3 = 0;
-        int opera4 = 0;
-        int trim = 0;
+        tabla [0] [2] = 471;
+        tabla [1] [2] = 355;
+        tabla [2] [2] = 474;
+        tabla [3] [2] = 161;
 
-        //Mensaje
-        System.out.println("El semestre que vendio mas de cada sabor.");
-        System.out.println("Presiona una tecla para continuar");
-
+        tabla [0] [3] = 427;
+        tabla [1] [3] = 158;
+        tabla [2] [3] = 160;
+        tabla [3] [3] = 308;
 
 
-        System.out.println("El trimestre Ene-Feb-Mar vendio un total de:" + opera1);
-        System.out.println("El trimestre Abr-May-Jun vendio un total de:" + opera2);
-        System.out.println("El trimestre Jul-Ago-Sep vendio un total de:" + opera3);
-        System.out.println("El trimestre Oct-Nov-Dic vendio un total de:" + opera4);
-        System.out.println("Lo que significa que en " + Fechas[trim] + " se vendio mas por " + minitrim);
+        for(int j = 0; j<3; j++)
+        {
+            if(tabla [0][j] > chocolate)
+            {
+                chocolate = tabla [0][j];
+            }
+        }
+        for(int j = 0; j<3; j++)
+        {
+            if(tabla [1][j] > vainilla)
+            {
+                vainilla = tabla [1][j];
+            }
+        }
+        for(int j = 0; j<3; j++)
+        {
+            if(tabla [2][j] > fresa)
+            {
+                fresa = tabla [2][j];
+            }
+        }
+        for(int j = 0; j<3; j++)
+        {
+            if(tabla [3][j] > oreo)
+            {
+                oreo = tabla [2][j];
+            }
+        }
+
+        System.out.println("Chocolate: Abr-May-Jun:" + chocolate);
+        System.out.println("Vainilla: Abr-May-Jun:" + vainilla);
+        System.out.println("Fresa: Jul-Ago-Sep:" + fresa);
+        System.out.println("Oreo: Ene-Feb-Mar:" + oreo);
+
+        trimestre = tabla [0][1] + tabla [1][1] + tabla [2][1] + tabla [3][1];
+        System.out.println("Trimestre que se vendio mas es: Abr-May-Jun:" + trimestre);
+
     }
 }

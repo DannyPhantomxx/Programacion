696da69d3fe47c3774e35d50e9b103a6ec46e9df
DannyPhantomxx <al04500293@tecmilenio.mx>
2022-11-25 19:31:36 -0600

    L

--- src/reto8/processing/Elevador.java
+++ src/reto8/processing/Elevador.java
@@ -1,4 +1,31 @@
 package reto8.processing;
 
 public class Elevador {
+    public static void main(String[] args){
+
+    }
+    private class controlador{
+    }
+
+    private class empleado{
+        String nombre;
+        int sn; //serial number
+        public empleado(String _nombre,int _sn){
+            nombre = _nombre;
+            sn = _sn;
+        }
+    }
+
+    private class elevador{
+        int id;
+        int peso;
+        int floor;
+
+        public elevador(int _id,int _peso,int _floor){
+            id = _id;
+            peso = _peso;
+            floor = _floor;
+        }
+    }
 }
+
